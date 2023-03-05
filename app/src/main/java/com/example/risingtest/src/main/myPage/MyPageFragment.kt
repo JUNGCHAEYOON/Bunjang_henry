7b8f6f187ec20950f7e27e78b269edb1939b272b
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-03-05 23:20:24 +0900

    0305 commit
    마이페이지 구현중

--- app/src/main/java/com/example/risingtest/src/main/myPage/MyPageFragment.kt
+++ app/src/main/java/com/example/risingtest/src/main/myPage/MyPageFragment.kt
@@ -3,6 +3,7 @@ package com.example.risingtest.src.main.myPage
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import com.bumptech.glide.Glide
 import com.example.risingtest.R
 import com.example.risingtest.config.BaseFragment
 import com.example.risingtest.databinding.FragmentMyPageBinding
@@ -13,10 +14,15 @@ class MyPageFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /* 툴바 버튼 */
         // 알림 액티비티로 이동
         binding.mypageBtnBell.setOnClickListener {
             val intent = Intent(context, AlimActivity::class.java)
             startActivity(intent)
         }
+
+        /* 동전 스핀 */
+        val myPageIvCoin = binding.mypageIvCoin
+        Glide.with(view.context).load(R.drawable.ic_coin_animation).into(myPageIvCoin)
     }
 }
