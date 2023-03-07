98f32f6e241515709ecad9ceabf525e32f0610ae
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-03-08 01:22:02 +0900

    0308 commit
    힘들다

--- app/src/main/java/com/example/risingtest/src/main/add/addBottomSheet/addBottomSheet.kt
+++ app/src/main/java/com/example/risingtest/src/main/add/addBottomSheet/addBottomSheet.kt
@@ -24,6 +24,11 @@ import com.google.android.material.navigation.NavigationBarMenu
 
 class addBottomSheet() : BottomSheetDialogFragment() {
 
+    var option_howmany = 1
+    var option_status = true        // true 중고, false 새상품
+    var option_getBack = true       // true 불가, false 가능
+    var option_location = "마포"     //디폴트 마포! 추후 선택지 설정예정
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,12 +45,14 @@ class addBottomSheet() : BottomSheetDialogFragment() {
         val sangtaeOp1 = view.findViewById<TextView>(R.id.add_btn_junggo)
         val sangtaeOp2 = view.findViewById<TextView>(R.id.add_btn_saesangpum)
         sangtaeOp1.setOnClickListener {
+            option_status = true
             sangtaeOp1.setTextColor(Color.parseColor("#ff0000"))
             sangtaeOp1.setBackgroundResource(R.drawable.radius_whitepink)
             sangtaeOp2.setTextColor(Color.parseColor("#AAAAAA"))
             sangtaeOp2.setBackgroundResource(R.drawable.radius_mypage_point)
         }
         sangtaeOp2.setOnClickListener {
+            option_status = false
             sangtaeOp2.setTextColor(Color.parseColor("#ff0000"))
             sangtaeOp2.setBackgroundResource(R.drawable.radius_whitepink)
             sangtaeOp1.setTextColor(Color.parseColor("#AAAAAA"))
@@ -56,12 +63,14 @@ class addBottomSheet() : BottomSheetDialogFragment() {
         val gyohwanOp1 = view.findViewById<TextView>(R.id.add_btn_bulga)
         val gyohwanOp2 = view.findViewById<TextView>(R.id.add_btn_ganung)
         gyohwanOp1.setOnClickListener {
+            option_getBack = true
             gyohwanOp1.setTextColor(Color.parseColor("#ff0000"))
             gyohwanOp1.setBackgroundResource(R.drawable.radius_whitepink)
             gyohwanOp2.setTextColor(Color.parseColor("#AAAAAA"))
             gyohwanOp2.setBackgroundResource(R.drawable.radius_mypage_point)
         }
         gyohwanOp2.setOnClickListener {
+            option_getBack = false
             gyohwanOp2.setTextColor(Color.parseColor("#ff0000"))
             gyohwanOp2.setBackgroundResource(R.drawable.radius_whitepink)
             gyohwanOp1.setTextColor(Color.parseColor("#AAAAAA"))
@@ -72,8 +81,13 @@ class addBottomSheet() : BottomSheetDialogFragment() {
             //todo
         }
 
+        /* 이 프래그먼트에서 dismiss 와 동시에 addAdctivity 로 변수를 넘겨줍니다. 해당 변수는
+        * 액티비티로 넘어간뒤 정제되어 텍스트뷰에 들어가고 이 데이터들이 일렬화 되어 post 됩니다.
+        * */
         // 선택 완료 버튼
         view.findViewById<TextView>(R.id.add_btn_finish).setOnClickListener {
+            option_howmany = view.findViewById<TextView>(R.id.add_etv_howmany).text.toString().toInt()
+
             dismiss()
         }
 
